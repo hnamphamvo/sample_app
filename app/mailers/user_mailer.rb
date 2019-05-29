e7cb8591fa6121f6a93742c504113d3ee2310d7f
hnamphamvo <hnamphamvo@gmail.com>
2019-05-29 11:35:03 +0700

    Chapter 11: Account activation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# :nodoc:
+class UserMailer < ApplicationMailer
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: t "mail.subject"
+  end
+
+  def password_reset
+    @greeting = t "mail.greeting"
+    mail to: "to@example.org"
+  end
+end
