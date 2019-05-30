443b5439c90b30b8c77657cee6d09267c61616d3
hnamphamvo <hnamphamvo@gmail.com>
2019-05-30 16:20:23 +0700

    Chapter 9: Advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+rrequire 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
@@ -53,5 +53,8 @@ class UserTest < ActiveSupport::TestCase
   test 'password should have a minimum length' do
     @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
+  end
+  test 'authenticated? should return false for a user with nil digest' do
+    assert_not @user.authenticated?('')
   end
 end
